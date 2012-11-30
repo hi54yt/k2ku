c80c787aace96c6a9cf962e21adc89108e492568
Yang tao <emcse@163.com>
2012-11-30 20:33:00 +0800

    add show page for a post

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121129051109) do
+ActiveRecord::Schema.define(:version => 20121130120712) do
+
+  create_table "comments", :force => true do |t|
+    t.integer  "post_id"
+    t.text     "content"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "posts", :force => true do |t|
     t.string   "url"
